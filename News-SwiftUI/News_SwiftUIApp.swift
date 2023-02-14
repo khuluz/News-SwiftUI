49131d60253fd878436a1f5e94c41450e35902f5
khuluz <lindokuhle.zungu@outlook.com>
2023-02-14 10:40:04 +0200

    Initial Commit

---
+++ News-SwiftUI/News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_SwiftUIApp.swift
+//  News-SwiftUI
+//
+//  Created by Lindo on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
